3f8e2186433be09bb76ab019d966489f25727781
claranceatgalvanize <claranceatgalvanize@gmail.com>
2020-04-29 07:52:36 -0700

    optimized the code to use a loop generated list of stars and numbers instead of hard coding them in. and also turned the stars variable into a react state property using hooks

--- src/start-match-com/StartMatch.js
+++ src/start-match-com/StartMatch.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./StartMatch.scss";
 
 export const StartMatch = () => {
+  const [stars, setStars] = useState(utils.random(1, 9));
   return (
     <div>
       <div className="game">
@@ -10,26 +11,16 @@ export const StartMatch = () => {
         </div>
         <div className="body">
           <div className="left">
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
+            {utils.range(1, stars).map((starId) => (
+              <div key={starId} className="star" />
+            ))}
           </div>
           <div className="right">
-            <button className="number">1</button>
-            <button className="number">2</button>
-            <button className="number">3</button>
-            <button className="number">4</button>
-            <button className="number">5</button>
-            <button className="number">6</button>
-            <button className="number">7</button>
-            <button className="number">8</button>
-            <button className="number">9</button>
+            {utils.range(1, 9).map((number) => (
+              <button key={number} className="number">
+                {number}
+              </button>
+            ))}
           </div>
         </div>
         <div className="timer">Time Remaining: 10</div>
