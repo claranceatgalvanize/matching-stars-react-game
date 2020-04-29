827e981fa6049f631c3c124159873b434b192545
claranceatgalvanize <claranceatgalvanize@gmail.com>
2020-04-29 07:27:59 -0700

    added skeleton and math science

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.scss";
+import { StartMatch } from "./start-match-com/StartMatch";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <StartMatch />
+    </div>
+  );
 }
 
 export default App;
